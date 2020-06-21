c815d3199e517e48218f4e9ce85a223f7078e171
miguelsantos69 <kuba16_1900@o2.pl>
2020-06-21 17:04:26 +0200

    add TronController with functions create wallet and get it balance

---
+++ app/Http/Controllers/API/TronController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TronController extends Controller
+{
+
+    /**
+     * Function to create a new wallet with it address and secret
+     *
+     */
+    public function createWallet()
+    {
+        //
+    }
+
+    /**
+     * Function to show balance of specific wallet
+     *
+     */
+    public function getWalletBalance()
+    {
+        //
+    }
+
+}
